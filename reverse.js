b1816b2873747b286f3731d437d34541f1159332
Maria Estrella <98559654+mlestrella843@users.noreply.github.com>
2025-02-11 12:03:53 -0700

    Metodos split, reverse, and join

---
+++ reverse.js
@@ -0,0 +1,26 @@
+// Reversing an array
+
+const fruits = ["Mango", "Grape", "Apple", "Pineapple"]
+
+const reverseFruits = fruits.reverse();
+
+console.log(reverseFruits); // ["Pineapple", "Apple", "Grape", "Mango"]
+
+
+// Reversing a string
+
+const word = "Hello"
+
+console.log(word)
+
+const reverseWorld = word.split("");
+
+console.log(reverseWorld);
+
+const reverseWorld1 = reverseWorld.reverse();
+
+console.log(reverseWorld1);
+
+const reverseWord2 = reverseWorld1.join("");
+
+console.log(reverseWord2); // "olleH"

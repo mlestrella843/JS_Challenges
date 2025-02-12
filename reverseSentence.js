75ce94a75c5124553a9ef1e6a82575cfa077c4c5
Maria Estrella <98559654+mlestrella843@users.noreply.github.com>
2025-02-11 21:00:36 -0700

    Fixing some words

--- reverseSentence.js
+++ reverseSentence.js
@@ -4,18 +4,18 @@
 const sentence = "Hello I'm Maria Estrella, nice to meet you";
 console.log(sentence);
 
-// 1. I converted the sentence into an arrays of words.
-const arrayWords = sentence.split(" "); //Hice un arreglo con las palabras por separado.
+// 1. I converted the sentence into an array of words.
+const arrayWords = sentence.split(" "); //I created an array with separate words..
 console.log(arrayWords);
 
-// 2. I developed a function that take a word and reverses it.
+// 2. I developed a function that takes a word and reverses it.
 function reversedWords(array){
     const newArray = array.map( word => word.split("").reverse().join("")); //
     console.log(newArray); 
     return newArray;
 }
 
-//3. I Converted an array into a sentence again.
+//3. I Converted an array back into a sentence.
 const newSentence = reversedWords(arrayWords).join(" ");
 console.log(newSentence); // "olleH I'm ariaA estelM, ecnet ni ot emoc tnuy yeeg"
 
